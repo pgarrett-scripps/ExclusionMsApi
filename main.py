36981204df1ed8a9ecbebc1ec89c1d849d2b34eb
pgarrett <pgarrett@scripps.edu>
2023-01-23 13:03:48 -0800

    big refactor

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from __future__ import annotations
 
 import logging
 import os
-from typing import List
+from typing import List, Dict
 
-from fastapi import HTTPException, FastAPI, BackgroundTasks
-from fastapi.responses import FileResponse
+from fastapi import HTTPException, FastAPI
 
-from constants import DATA_FOLDER, PROCESS_CANDIDATES_FILE
+from constants import DATA_FOLDER
 from exclusionms.components import ExclusionInterval, ExclusionPoint
 from exclusionms.db import MassIntervalTree as ExclusionList
 from utils import Offset
@@ -25,62 +24,65 @@ def get_pickle_path(exclusion_list_name: str) -> str:
     return os.path.join(DATA_FOLDER, exclusion_list_name + '.pkl')
 
 
-@app.get("/", status_code=200)
-async def get_process_candidates_file():
-    return FileResponse(path=PROCESS_CANDIDATES_FILE, filename=PROCESS_CANDIDATES_FILE, media_type='text')
-
-
-# TODO: add exclusion list names to statistics
-@app.get("/exclusionms", status_code=200)
-async def get_exclusion_list_statistics():
+@app.get("/exclusionms/statistics", status_code=200)
+async def get_statistics() -> Dict:
     _log.info(f'Exclusion List Statistics')
+    return active_exclusion_list.stats()
+
+
+@app.get("/exclusionms/file", status_code=200)
+async def get_files() -> List[str]:
+    _log.info(f'Exclusion List Files')
     saved_files = os.listdir(DATA_FOLDER)
     saved_files_names = [''.join(f.split('.')[:-1]) for f in saved_files]
-    return {'files': saved_files_names, 'active_exclusion_list': active_exclusion_list.stats(), 'Offsets:': offset}
+    return saved_files_names
 
 
-# TODO: Add in merge command
-@app.post("/exclusionms", status_code=200)
-async def save_load_active_exclusion_list(save: bool, exclusion_list_name: str):
-    pickle_path = get_pickle_path(exclusion_list_name)
-    _log.info(f'pickle_path: {pickle_path}')
+@app.post("/exclusionms/save", status_code=200)
+async def save(exid: str):
+    pickle_path = get_pickle_path(exid)
 
-    if save:
-        _log.info(f'Save Exclusion List')
-        if os.path.exists(pickle_path):
-            _log.warning(f'{pickle_path} already exists. Overriding.')
+    _log.info(f'Save Exclusion List')
+    if os.path.exists(pickle_path):
+        _log.warning(f'{pickle_path} already exists. Overriding.')
 
-        try:
-            active_exclusion_list.save(pickle_path)
-        except Exception as e:
-            _log.error(f'Error when saving exclusion list: {e}')
-            raise HTTPException(status_code=500, detail='Error saving active exclusion list.')
-
-    else:
-        _log.info(f'Load Exclusion List')
-        if not os.path.exists(pickle_path):
-            raise HTTPException(status_code=404, detail=f"exclusion list with name: {exclusion_list_name} not found.")
-
-        try:
-            active_exclusion_list.load(pickle_path)
-        except Exception as e:
-            _log.error(f'Exception when loading exclusion list: {e}', exc_info=True)
-            raise HTTPException(status_code=500, detail='Error loading active exclusion list.')
+    try:
+        active_exclusion_list.save(pickle_path)
+    except Exception as e:
+        _log.error(f'Error when saving exclusion list: {e}')
+        raise HTTPException(status_code=500, detail='Error saving active exclusion list.')
 
 
-@app.delete("/exclusionms", status_code=200)
-async def clear_active_exclusion_list():
+@app.post("/exclusionms/load", status_code=200)
+async def load(exid: str):
+    pickle_path = get_pickle_path(exid)
+
+    _log.info(f'Load Exclusion List')
+    if not os.path.exists(pickle_path):
+        raise HTTPException(status_code=404, detail=f"exclusion list with name: {exid} not found.")
+
+    try:
+        active_exclusion_list.load(pickle_path)
+    except Exception as e:
+        _log.error(f'Exception when loading exclusion list: {e}', exc_info=True)
+        raise HTTPException(status_code=500, detail='Error loading active exclusion list.')
+
+
+@app.post("/exclusionms/clear", status_code=200)
+async def clear() -> int:
     _log.info(f'Delete Active Exclusion List')
+    num_intervals_cleared = len(active_exclusion_list)
     active_exclusion_list.clear()
+    return num_intervals_cleared
 
 
-@app.delete("/exclusionms/file", status_code=200)
-async def delete_exclusion_list_save(exclusion_list_name: str):
+@app.post("/exclusionms/delete", status_code=200)
+async def delete(exid: str):
     _log.info(f'Delete Exclusion List Save')
-    pickle_path = get_pickle_path(exclusion_list_name)
+    pickle_path = get_pickle_path(exid)
 
     if not os.path.exists(pickle_path):
-        raise HTTPException(status_code=404, detail=f"exclusion list with name: {exclusion_list_name} not found.")
+        raise HTTPException(status_code=404, detail=f"exclusion list with name: {exid} not found.")
 
     try:
         os.remove(pickle_path)
@@ -89,25 +91,8 @@ async def delete_exclusion_list_save(exclusion_list_name: str):
         raise HTTPException(status_code=500, detail='Error deleting exclusion list.')
 
 
-@app.get("/exclusionms/file", status_code=200)
-async def download_exclusion_list_save(exclusion_list_name: str):
-    _log.info(f'Download Exclusion List')
-    pickle_path = get_pickle_path(exclusion_list_name)
-
-    if not os.path.exists(pickle_path):
-        raise HTTPException(status_code=404, detail=f"exclusion list with name: {exclusion_list_name} not found.")
-
-    return FileResponse(path=pickle_path)
-
-
-@app.post("/exclusionms/file", status_code=200)
-async def upload_exclusion_list_save(file):
-    _log.info(f'Upload Exclusion List')
-    return
-
-
-@app.post("/exclusionms/interval_search", response_model=List[List[ExclusionInterval]], status_code=200)
-async def get_intervals(exclusion_intervals: List[ExclusionInterval]):
+@app.post("/exclusionms/intervals/search", response_model=List[List[ExclusionInterval]], status_code=200)
+async def search_intervals(exclusion_intervals: List[ExclusionInterval]):
     for exclusion_interval in exclusion_intervals:
         if not exclusion_interval.is_valid():
             raise HTTPException(status_code=400,
@@ -116,7 +101,7 @@ async def get_intervals(exclusion_intervals: List[ExclusionInterval]):
     return [active_exclusion_list.query_by_interval(exclusion_interval) for exclusion_interval in exclusion_intervals]
 
 
-@app.post("/exclusionms/interval", response_model=None, status_code=200)
+@app.post("/exclusionms/intervals", response_model=None, status_code=200)
 async def add_intervals(exclusion_intervals: List[ExclusionInterval]):
     for exclusion_interval in exclusion_intervals:
         if not exclusion_interval.is_valid():
@@ -125,8 +110,8 @@ async def add_intervals(exclusion_intervals: List[ExclusionInterval]):
     _ = [active_exclusion_list.add(exclusion_interval) for exclusion_interval in exclusion_intervals]
 
 
-@app.delete("/exclusionms/interval", response_model=List[List[ExclusionInterval]], status_code=200)
-async def remove_intervals(exclusion_intervals: List[ExclusionInterval]):
+@app.delete("/exclusionms/intervals", response_model=List[List[ExclusionInterval]], status_code=200)
+async def delete_intervals(exclusion_intervals: List[ExclusionInterval]):
     for exclusion_interval in exclusion_intervals:
         if not exclusion_interval.is_valid():
             raise HTTPException(status_code=400,
@@ -144,24 +129,21 @@ def apply_offset(point: ExclusionPoint, offset: Offset):
     if point.intensity:
         point.intensity += offset.intensity
 
-@app.post("/exclusionms/point", response_model=List[List[ExclusionInterval]], status_code=200)
-async def get_points(exclusion_points: list[ExclusionPoint]):
+
+@app.post("/exclusionms/points/search", response_model=List[List[ExclusionInterval]], status_code=200)
+async def search_points(exclusion_points: list[ExclusionPoint]):
     for point in exclusion_points:
         apply_offset(point, offset)
 
-    return [active_exclusion_list.query_by_point(point) for point in exclusion_points]
+    return [list(active_exclusion_list.query_by_point(point)) for point in exclusion_points]
 
-@app.post("/exclusionms/process_candidates", response_model=List[bool], status_code=200)
-async def get_points(exclusion_points: list[ExclusionPoint]):
+
+@app.post("/exclusionms/points/exclusion_search", response_model=List[bool], status_code=200)
+async def exclusion_search_points(exclusion_points: list[ExclusionPoint]):
     for point in exclusion_points:
         apply_offset(point, offset)
 
     return [active_exclusion_list.is_excluded(point) for point in exclusion_points]
-
-
-@app.get("/exclusionms/stats", status_code=200)
-async def get_statistics():
-    return active_exclusion_list.stats()
 
 
 @app.get("/exclusionms/offset", status_code=200)
